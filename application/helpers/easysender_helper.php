26738d0bb4da23eba4bedf9b209273ce4a575275
easydigitalgit <easydigitalgit@gmail.com>
2024-01-27 16:35:15 +0700

    remove key

--- application/helpers/easysender_helper.php
+++ application/helpers/easysender_helper.php
@@ -1,30 +1,31 @@
- <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
- /**
- * Function Name
- *
- * Function description
- *
- * @access	public
- * @param	type	name
- * @return	type	
- * 
- * 
- * multidevice : f6fe36fb61a12e786203324c25ab55b6e02ae8ad
- */
- 
-if (! function_exists('sendtext'))
-{
-	function sendtext($toNumber,$text,$senderNumber)
-	{
-      $data = [
-                'api_key' => 'b2d95af932eedb4de92b3496f338aa5f97b36ae0',
-                'sender'  => $senderNumber,
-                'number'  => $toNumber,
-                'message' => $text
-              ];
+ <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+  /**
+   * Function Name
+   *
+   * Function description
+   *
+   * @access	public
+   * @param	type	name
+   * @return	type	
+   * 
+   * 
+   * multidevice : f6fe36fb61a12e786203324c25ab55b6e02ae8ad
+   */
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
+  if (!function_exists('sendtext')) {
+    function sendtext($toNumber, $text, $senderNumber)
+    {
+      $data = [
+        'api_key' => '',
+        'sender'  => $senderNumber,
+        'number'  => $toNumber,
+        'message' => $text
+      ];
+
+      $curl = curl_init();
+      curl_setopt_array(
+        $curl,
+        array(
           CURLOPT_URL => "https://sender.easydigital.id/app/api/send-message",
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => "",
@@ -33,68 +34,72 @@ if (! function_exists('sendtext'))
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => json_encode($data))
-        );
+          CURLOPT_POSTFIELDS => json_encode($data)
+        )
+      );
 
-        $response = curl_exec($curl);
+      $response = curl_exec($curl);
 
-        curl_close($curl);
-        return $response;
+      curl_close($curl);
+      return $response;
     }
-}
+  }
 
 
 
-if (! function_exists('sendmedia'))
-{
-  function sendmedia($toNumber,$caption,$linkFile,$senderNumber)
-  {
+  if (!function_exists('sendmedia')) {
+    function sendmedia($toNumber, $caption, $linkFile, $senderNumber)
+    {
       $data = [
-              'api_key' => 'b2d95af932eedb4de92b3496f338aa5f97b36ae0',
-              'sender'  => $senderNumber,
-              'number'  => $toNumber,
-              'message' => $caption,
-              
-              'url' => $linkFile //link Image or link PDF
-              ];
+        'api_key' => '',
+        'sender'  => $senderNumber,
+        'number'  => $toNumber,
+        'message' => $caption,
 
-          $curl = curl_init();
-          curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://sender.easydigital.id/app/api/send-media",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($data))
-          );
+        'url' => $linkFile //link Image or link PDF
+      ];
 
-          $response = curl_exec($curl);
+      $curl = curl_init();
+      curl_setopt_array(
+        $curl,
+        array(
+          CURLOPT_URL => "https://sender.easydigital.id/app/api/send-media",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => json_encode($data)
+        )
+      );
 
-          curl_close($curl);
-         
-        return $response;
+      $response = curl_exec($curl);
+
+      curl_close($curl);
+
+      return $response;
     }
-}
+  }
 
-if (! function_exists('sendbutton'))
-{
-  function sendbutton($toNumber,$text,$footer,$button1, $button2,$senderNumber)
-  {
+  if (!function_exists('sendbutton')) {
+    function sendbutton($toNumber, $text, $footer, $button1, $button2, $senderNumber)
+    {
       $data = [
-              'api_key' => 'b2d95af932eedb4de92b3496f338aa5f97b36ae0',
-              'sender'  => $senderNumber,
-              'number'  => $toNumber,
-              'message' => $text,
-              'footer' => $footer,
-              'button1' => $button1,
-              'button2' => $button2,
-          ];
+        'api_key' => '',
+        'sender'  => $senderNumber,
+        'number'  => $toNumber,
+        'message' => $text,
+        'footer' => $footer,
+        'button1' => $button1,
+        'button2' => $button2,
+      ];
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
+      $curl = curl_init();
+      curl_setopt_array(
+        $curl,
+        array(
           CURLOPT_URL => "https://sender.easydigital.id/app/api/send-button",
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => "",
@@ -103,28 +108,26 @@ if (! function_exists('sendbutton'))
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => json_encode($data))
-        );
+          CURLOPT_POSTFIELDS => json_encode($data)
+        )
+      );
 
-        $response = curl_exec($curl);
+      $response = curl_exec($curl);
 
-        curl_close($curl);
-         
-        return $response;
+      curl_close($curl);
+
+      return $response;
     }
-}
-
-
-if (! function_exists('msgTemplate'))
-{
-  function msgTemplate($nama='')
-  {
-
-    $ret = '';
-
-    $ret .= 'Terima kasih kepada bapak/ibu '.$nama.' yang telah menggunakan layanan Ambulan Hidayatullah, Dukung kami agar terus bermanfaat untuk semua.';
-    return $ret;
-
-
   }
-}
+
+
+  if (!function_exists('msgTemplate')) {
+    function msgTemplate($nama = '')
+    {
+
+      $ret = '';
+
+      $ret .= 'Terima kasih kepada bapak/ibu ' . $nama . ' yang telah menggunakan layanan Ambulan Hidayatullah, Dukung kami agar terus bermanfaat untuk semua.';
+      return $ret;
+    }
+  }
